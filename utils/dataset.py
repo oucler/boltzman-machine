bd2b5e2d4f1d041d51c4d91fae137814b0d1208a
oucler <onurucler@yahoo.com>
2024-02-04 20:55:52 -0800

    mnist data read is added.

--- utils/dataset.py
+++ utils/dataset.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 class MNISTLoader:
-    def __init__(self, path='.'):
+    def __init__(self, path='.', dataset_name='mnist'):
         self.path = path
         self.dir_path = os.path.join(self.path, 'mnist/')
         self.training_path_dir = os.path.join(self.dir_path, 'train-images-idx3-ubyte')
@@ -25,15 +25,14 @@ class MNISTLoader:
     @staticmethod
     def target_data(target_data_dir):
         with open(target_data_dir, 'rb') as target_data:
+            magic, n_samples = struct.unpack(">II", target_data.read(8))
             target = np.fromfile(target_data, dtype=np.int8)
         return target
 
 
 if __name__ == "__main__":
     loader = MNISTLoader(path='../data/')
-    # X, y = loader.load(mode='train')
-    # X_test, y_test = loader.load(mode='test')
-    # print(X.shape, y.shape, X_test.shape, y_test.shape)
-    Xt, Xtest = loader.training_data(loader.training_path_dir), loader.training_data(loader.test_path_dir)
-    yt, ytest = loader.target_data(loader.training_path_target_dir), loader.target_data(loader.test_path_target_dir)
-    print(Xt.shape, yt.shape, Xtest.shape, ytest.shape)
+    X, X_test = loader.training_data(loader.training_path_dir), loader.training_data(loader.test_path_dir)
+    y, y_test = loader.target_data(loader.training_path_target_dir), loader.target_data(loader.test_path_target_dir)
+    print(f"Training data shape: {X.shape}, label shape: {y_test.shape}"
+          f"test data shape: {X_test.shape}, test label shape: {y_test.shape}")
